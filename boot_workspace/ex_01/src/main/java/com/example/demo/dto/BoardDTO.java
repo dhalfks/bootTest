e6e027c821df90bf119f4509a965671f0b8e700c
dhalfks <miran5093@gmail.com>
2023-08-22 08:56:38 +0900

    board register check

---
+++ boot_workspace/ex_01/src/main/java/com/example/demo/dto/BoardDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BoardDTO {
+
+	private Long bno;
+	private String title;
+	private String writer;
+	private String content;
+	private LocalDateTime regAt, modAt;
+}
